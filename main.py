5143c7f093bceb2d4f7b658a17aea2f214bc4810
NicolasSKing <nocoben10@gmail.com>
2026-06-04 11:34:27 +0800

    now_using_the_getter_method_this_will_display_all_the_pets_information

--- main.py
+++ main.py
@@ -9,3 +9,8 @@ age = int(input("Enter your pet's age: "))
 my_pet.set_name(name)
 my_pet.set_animal_type(animal_type)
 my_pet.set_age(age)
+
+print("\nPet Information")
+print(f"Name: {my_pet.get_name()}")
+print(f"Type: {my_pet.get_animal_type()}")
+print(f"Age: {my_pet.get_age()}")
