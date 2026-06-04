e82a19503d067bd049a5929abd817488634390e6
NicolasSKing <nocoben10@gmail.com>
2026-06-04 11:32:13 +0800

    this_will_store_all_the_value_of_the_users_inputs_using_the_setter_method

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ my_pet = Pet()
 name = input("Enter your pet's name: ")
 animal_type = input("Enter your pet's type: ")
 age = int(input("Enter your pet's age: "))
+
+my_pet.set_name(name)
+my_pet.set_animal_type(animal_type)
+my_pet.set_age(age)
